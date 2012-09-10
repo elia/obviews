d87816a866db68c5b27909e61c2c97da26955617
Elia Schito <elia@schito.me>
2012-09-10 22:13:54 +0200

    Let pass @ivars for Rails own actions (e.g. error rescuers)

--- lib/obviews/view.rb
+++ lib/obviews/view.rb
@@ -3,11 +3,16 @@ module Obviews
     extend ActiveSupport::Concern
     
     included do
-      
+      alias assign_without_obviews assign
+
       # Force this method definition since it's not in a module...
       def assign(new_assigns)
-        @_assigns = new_assigns #.tap{ |a| a.keys.each { |k| a[k] } }
-        new_assigns.keys.each do |key|
+        keys = new_assigns.symbolize_keys.keys
+        exception_keys = [:application_trace, :exception, :framework_trace, :full_trace, :request]
+        return assign_without_obviews(new_assigns) if keys.sort == exception_keys
+
+        @_assigns = new_assigns
+        keys.each do |key|
           define_singleton_method(key) { @_assigns[key] }
         end
       end
